5e37a3cc4e99fd0d8968c981f5e5b14e855d119f
Nino K <helsincraft@gmail.com>
2024-08-26 14:08:05 +0300

    code refactoring and config changes and new features and modernizing code and yeah

--- src/main/kotlin/fi/assyt/kysplugin/MainClass.kt
+++ src/main/kotlin/fi/assyt/kysplugin/MainClass.kt
@@ -1,31 +1,38 @@
 package fi.assyt.kysplugin
 
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Bukkit
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
+import org.bukkit.command.ConsoleCommandSender
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
-class MainClass : JavaPlugin() {
+class MainClass : JavaPlugin(), CommandExecutor {
 
     override fun onEnable() {
         // Plugin startup logic
-        getCommand("kys")?.setExecutor(KysCommand())
+        getCommand("kys")?.setExecutor(this)
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
         logger.info("so... you killed me?")
     }
-}
-
-class KysCommand : CommandExecutor {
 
     override fun onCommand(sender: CommandSender, cmd: Command, label: String, args: Array<String>): Boolean {
         if (sender is Player) {
             sender.health = 0.0
-            Bukkit.broadcastMessage("so " + sender.name + " decided to kill themselves (but why tho)")
+            Bukkit.getServer().broadcast(Component.text("so " + sender.name + " decided to kill themselves ").append(Component.text("(but why tho)", NamedTextColor.DARK_RED)))
+        } else if (sender is ConsoleCommandSender) {
+            sender.sendMessage("aight bet")
+            Bukkit.getServer().broadcast(Component.text("server is shutting down"))
+
+            Bukkit.shutdown()
+        } else {
+            sender.sendMessage("what are you? a mob?")
         }
 
         return true
