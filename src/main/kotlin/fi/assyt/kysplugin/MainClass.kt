24553a722640fd9c61b72041e81fb597084bf11b
Nino K <helsincraft@gmail.com>
2024-08-31 09:11:13 +0300

    code cleanup

--- src/main/kotlin/fi/assyt/kysplugin/MainClass.kt
+++ src/main/kotlin/fi/assyt/kysplugin/MainClass.kt
@@ -3,8 +3,8 @@ package fi.assyt.kysplugin
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Bukkit
-import org.bukkit.command.CommandExecutor
 import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.command.ConsoleCommandSender
 import org.bukkit.entity.Player
@@ -25,7 +25,10 @@ class MainClass : JavaPlugin(), CommandExecutor {
     override fun onCommand(sender: CommandSender, cmd: Command, label: String, args: Array<String>): Boolean {
         if (sender is Player) {
             sender.health = 0.0
-            Bukkit.getServer().broadcast(Component.text("so " + sender.name + " decided to kill themselves ").append(Component.text("(but why tho)", NamedTextColor.DARK_RED)))
+            Bukkit.getServer().broadcast(
+                Component.text("so " + sender.name + " decided to kill themselves ")
+                    .append(Component.text("(but why tho)", NamedTextColor.DARK_RED))
+            )
         } else if (sender is ConsoleCommandSender) {
             sender.sendMessage("aight bet")
             Bukkit.getServer().broadcast(Component.text("server is shutting down"))
